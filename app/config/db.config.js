da8ce7a8326a4b8c88b2a41bd0a1a239f156525f
Allen Jamison <119152205+Teqnixx@users.noreply.github.com>
2024-07-18 23:01:05 +0800

    Update db.config.js

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "",
-    DB: "internnet_database"
+    HOST: "sql101.infinityfree.com",
+    USER: "if0_36835569",
+    PASSWORD: "hOls80fBqDsZHC",
+    DB: "if0_36835569_reactnative_database"
 };
